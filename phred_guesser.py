074a012dea75e4a378dc1d7a0f68b182bdf70225
Yu Fu <yfu@yfu.me>
2014-01-24 00:24:56 -0500

    report just phred33 or phred64

--- phred_guesser.py
+++ phred_guesser.py
@@ -15,12 +15,7 @@ def calc_qual_range(l):
             l_max = ord(c)
     return (l_min, l_max)
 
-RANGES = {
-    'Sanger': (33, 73),
-    'Solexa': (59, 104),
-    'Illumina-1.3': (64, 104),
-    'Illumina-1.5': (67, 104)
-}
+
 def main():
     counter = 0
     g_min = 255
@@ -36,6 +31,32 @@ def main():
                 g_max = l_max
             counter = 0
     print "The range of the quality scores is: [%d, %d]" % (g_min, g_max)
-        
+    print report(g_min, g_max)
+
+def report(g_min, g_max):
+    """Report phred33 or phred64
+    """
+    ranges = {
+        'phred33': (33, 84), # Sanger
+        # 'Solexa': (59, 104),
+        'phred64': (64, 115), # Illumina-1.3
+        # 'Illumina-1.5': (67, 104)
+    }
+    phred33_flag = False
+    phred64_flag = False
+    for k in ranges.keys():
+        if g_min >= ranges[k][0] and g_max <= ranges[k][1]:
+            phred33_flag = True
+        if g_min >= ranges[k][0] and g_max <= ranges[k][1]:
+            phred64_flag = True
+    if phred33_flag and phred64_flag:
+        print "Ambiguous"
+    elif phred33_flag and not phred64_flag:
+        print "phred33"
+    elif phred64_flag and not phred33_flag:
+        print "phred64"
+    else:
+        print "Something is wrong?"
+    
 if __name__ == "__main__":
     main()
