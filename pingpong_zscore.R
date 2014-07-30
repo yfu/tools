fc905972953aebfea9dda33ff3fea68d1a6425b2
Yu Fu <fuyu12345@gmail.com>
2014-07-30 09:50:31 -0400

    Adding some comments to pps.sh

--- pingpong_zscore.R
+++ pingpong_zscore.R
@@ -1,22 +1,32 @@
-sample.name <- "Phil.SRA.wt.ox.ovary.inserts"
+library(ggplot2)
+
+sample.name <- "Hi5.ox"
 a <- read.table("pp.dataframe")
 
 my.df <- data.frame(zscore=0)
 a <- cbind(a, my.df)
 
+pdf.name <- paste(sample.name, ".pp_raw_signal.pdf", sep="")
+pdf(pdf.name)
+p <- ggplot(a, aes(x=V1, y=V2)) + geom_line() + geom_point() + xlab("offset (10 means there is a 10-nt overlap)") + ylab("raw signal") + ggtitle(sample.name)
+print(p)
+dev.off()
+
 for (i in 1:nrow(a)) {
-    my.rows <- c(1:(i-1), (i+1):nrow(a))
+    my.rows <- 1:nrow(a)
+    my.rows <- my.rows[-i]
     my.num <- a[my.rows, 2]
     my.mean <- mean(my.num)
     my.sd <- sd(my.num)
     z <- (a[i, 2] - my.mean) / my.sd
+    # message(my.sd)
     if(is.na(z)) { z = 0 }
     a[i, 3] = z
 }
 
 library(ggplot2)
 
-pdf.name <- paste(sample.name, ".pp_signal.pdf", sep="")
+pdf.name <- paste(sample.name, ".pp_zscore.pdf", sep="")
 pdf(pdf.name)
-ggplot(a, aes(x=V1, y=zscore)) + geom_line() + xlab("offset (10 means there is a 10-nt overlap)") + ylab("z-score") + ggtitle(sample.name)
+ggplot(a, aes(x=V1, y=zscore)) + geom_line() + geom_point() + xlab("offset (10 means there is a 10-nt overlap)") + ylab("z-score") + ggtitle(sample.name)
 dev.off()
--- pps.sh
+++ pps.sh
@@ -4,8 +4,14 @@
 # Accept bed2 as input
 # Yu Fu (yfu at yfu dot me)
 
+# Usage: put a genome.fa containing the genome sequence and a bed2 file containing the alignment. 
+# A Bed2 file is just like a normal bed file, but it has the # copies as the 4th column and the NTM as the 5th column
+# TODO: include a script (bed_2_bed2.py) that, given the bed file (converted from sam file), outputs a bed2 file.
+
 ext5=30;
 ext3=30
+cpu=8
+
 # awk -v ext=$ext 'BEGIN{OFS="\t" } { $2-=ext; if($2<0) {$2=0}; print  }' r1.no_adaptor_hi5_unox.1mm.bed2 | head
 
 # TODO: replace bedtools slop with with my own implementation, which is able to tell:
@@ -17,7 +23,7 @@ input=$1
 
 # If the genome length file does not exist, I'll create one.
 if [ ! -f genome.length ]; then 
-    python get_fasta_seq_length.py genome.fa > genome.length
+    get_fasta_seq_length.py genome.fa > genome.length
 fi
 
 bedtools slop -s -l ${ext5} -r ${ext3} -i ${input} -g genome.length > ext.tmp.bed2
@@ -33,12 +39,12 @@ fi
 cat ${input} | awk 'BEGIN{u="_"} { print ">" $1 u $2 u $3 u $4 u $5 u $6 "\t"; print $7}' > orig_reads
 
 # TODO: remove -m 100 to see if this affects the final result.
-bowtie -p 30 -f -v 0 -a -m 100 --best --strata -S ext.idx orig_reads | samtools view -Sb - -F0x4 > mapping_result.bam
+bowtie -p ${cpu} -f -v 0 -a -m 100 --best --strata -S ext.idx orig_reads | samtools view -Sb - -F0x4 > mapping_result.bam
 samtools sort -@ 20 -m 2G mapping_result.bam  mapping_result.sorted && rm mapping_result.bam
 samtools view -b -h -f 0x10 mapping_result.sorted.bam > mapping_result.reverse_strand.sorted.bam
 
 # Get NTM for each reads
 # samtools view mapping_result.reverse_strand.sorted.bam | awk '{ print $10 "\t" $3 }' | sort -k1 | uniq | cut -f1 | uniq -c | sed -r 's/\s+([0-9]+)\s+([ATCGN]+)/\2\t\1/' > ntm_reads_mapped_to_reverse_index.txt
 samtools view mapping_result.reverse_strand.sorted.bam > mapping_result.reverse_strand.noheader.sorted.sam
-
+pp_hist.py mapping_result.reverse_strand.noheader.sorted.sam >pp.log 2>pp.err
 # samtools view mapping_result.reverse_strand.sorted.bam | python pp_hist.py ntm_reads_mapped_to_reverse_index.txt
