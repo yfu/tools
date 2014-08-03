ec99c4288c63cbfb46eb02dd67ec73b8b54306c7
Yu Fu <fuyu12345@gmail.com>
2014-08-03 18:07:54 -0400

    fasta_lengths.pl will throw out an error when the fasta contains whitespaces

--- fasta_lengths.pl
+++ fasta_lengths.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/env perl
 
 while(<>) {
-    chomp; if(m/^>(.+)/){$id=$1} else{ $l{$id} += length($_); } 
+    chomp;
+    if(m/\s/) {
+	print("No whitespaces allowed in the fasta headers. Consider using cut -f1 to modify the headers. Bye!\n");
+	exit 1;
+    }
+    if(m/^>(.+)/){$id=$1} else{ $l{$id} += length($_); } 
 }
 
 for my $i (keys %l) {
