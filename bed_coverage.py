781662c334b2188b2150608ae8d1ca43e349ea92
Yu Fu <fuyu12345@gmail.com>
2014-08-05 19:19:03 -0400

    bed_coverage.py can calculate the coverage based on the signal.

---
+++ bed_coverage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Given a bed2 file from STDIN, this script will take the # of copies and the NTMs into consideration 
+# and generate the coverage for all chromosomes (or, contigs, in my case)...
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='A tools to calculate bed2 coverage')
+
+parser.add_argument('-s', '--strand', help='The fasta file', required=True)
+
+args = parser.parse_args()
+requested_strand = args.strand
+coverages = {}
+
+for line in sys.stdin.readlines():
+    line = line.strip()
+    line = line.split()
+    chrom = line[0]
+    start = int(line[1])
+    end = int(line[2])
+    copy = float(line[3])
+    ntm = float(line[4])
+    strand = line[5]
+    if(strand != requested_strand):
+        continue
+    seq = line[6]
+    
+    effective_copy = copy / ntm
+    if chrom not in coverages.keys():
+        coverages[chrom] = {}
+    coverages[chrom][1] = 2
+
+    for i in range(start, end):
+        if i in coverages[chrom].keys():
+            coverages[chrom][i] += effective_copy
+        else: 
+            coverages[chrom][i] = effective_copy           
+
+for i in sorted(coverages.keys()):
+    cur_dict = coverages[i]
+    m = max(cur_dict.keys())
+    for j in range(0, m+1):
+        if j not in cur_dict.keys():
+            print str(i) + "\t" + str(j) + "\t" + str(0)
+        else:
+            print str(i) + "\t" + str(j) + "\t" + str(cur_dict[j])
+
+
