92f41232882a40cefb7fe2b59352edc62bf69e95
Yu Fu <fuyu12345@gmail.com>
2014-09-14 16:30:43 -0400

    have not finished fasta_sampler.py yet

--- fasta_sampler.py
+++ fasta_sampler.py
@@ -3,4 +3,10 @@
 # This is a simple script that, given a fasta file and the corresponding fasta index file,
 # generates a set of random ranges of length specified by the user.
 
+# Usage: fasta_sampler.py 100 your.fa
 
+# import sys
+# fh = open(sys.argv[1])
+# for line in fh.readlines():
+    
+# Not finished
