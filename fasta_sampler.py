af362dac9047e0836cae1ef037741067049ee4a5
Yu Fu <yfu@yfu.me>
2014-09-14 16:22:17 -0400

    added test data: fasta_splitter_test_data.fa

---
+++ fasta_sampler.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# This is a simple script that, given a fasta file and the corresponding fasta index file,
+# generates a set of random ranges of length specified by the user.
+
+
