1b426339869541bb7cb9ec7c4c8a7d469cd700d7
Yu Fu <fuyu12345@gmail.com>
2014-10-27 00:44:46 -0400

    added a second parameter for ucsc_screenshots.sh

--- ucsc_screenshots.sh
+++ ucsc_screenshots.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 # Author: Yu Fu
-# Usage ucsc_screenshots.sh hgsid
+# Usage ucsc_screenshots.sh hgsid slop
+# Usage ucsc_screenshots.sh 393202989_aSfr1NFH0Tgwt9jhDFJzj3Ng8sBR 5000
 # Obtain the hgsid from the address bar (the hgsid is associated with all the tracks you have loaded)
-hgsid=393202989_aSfr1NFH0Tgwt9jhDFJzj3Ng8sBR
+# hgsid=393202989_aSfr1NFH0Tgwt9jhDFJzj3Ng8sBR
+# https://genome.ucsc.edu/cgi-bin/hgTracks?db=dm3&position=chr2R%3A2083755-2447312&hgsid=393202989_aSfr1NFH0Tgwt9jhDFJzj3Ng8sBR
 hgsid="$1"
+slop="$2"
+
 # db is the genome
 db=dm3
 
-ranges=( $(bedtools slop -i ~/data/piPipes/common/dm3/Brennecke.piRNAcluster.bed6.gz -g ~/data/piPipes/common/dm3/dm3.ChromInfo.txt -b 1000 | cut -f1-4 | grep -v 'X-TAS' | sed -E 's/([^\s]+)\s([^\s]+)\s([^\s]+)\s([^\s]+)/\1%3A\2-\3.\4/') )
+ranges=( $(bedtools slop -i ~/data/piPipes/common/dm3/Brennecke.piRNAcluster.bed6.gz -g ~/data/piPipes/common/dm3/dm3.ChromInfo.txt -b ${slop} | cut -f1-4 | grep -v 'X-TAS' | sed -E 's/([^\s]+)\s([^\s]+)\s([^\s]+)\s([^\s]+)/\1%3A\2-\3.\4/') )
 
 for i in "${ranges[@]}"; do
     # position="chr2R%3A2166393-2222282"
