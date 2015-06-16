32915324d32471c50fd16ed96d67fa3204341f90
Yu Fu <fuyu12345@gmail.com>
2015-06-16 13:12:05 -0400

    added scripts for comparison of normalization methods

---
+++ piPipes_aux/dm3.parameters.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export rRNA_MM=2
+export hairpin_MM=0
+export genome_MM=0
+export transposon_MM=2
+export siRNA_bot=20
+export siRNA_top=22
+export piRNA_bot=23
+export piRNA_top=29
