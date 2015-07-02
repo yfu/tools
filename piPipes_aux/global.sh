098c63d2c1ff145d1e40d9ad23482c750fa2fbc0
Yu Fu <yfu@yfu.me>
2015-07-02 16:21:19 -0400

    added Solexa_QA.pl

--- piPipes_aux/global.sh
+++ piPipes_aux/global.sh
@@ -4,3 +4,6 @@
 export BEDTOOLS=/home/fuy2/data/piPipes/bin/bedtools_piPipes
 export INSERT_BED_TO_BED2=/data/fuy2/piPipes/bin/piPipes_insertBed_to_bed2
 export BED2LENDIS
+
+# I added SolexaQA into repo/tools so this is no longer needed
+# perl $PIPELINE_DIRECTORY/bin/SolexaQA_piPipes.pl
