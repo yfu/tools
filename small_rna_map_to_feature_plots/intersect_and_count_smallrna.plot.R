e840155e0b402b8ece60b8b6b889ca7bfe3d103d
Yu Fu <fuyu12345@gmail.com>
2015-07-19 17:27:35 -0400

    intersect_and_count_smallrna.plot.R can now calculate p values

--- small_rna_map_to_feature_plots/intersect_and_count_smallrna.plot.R
+++ small_rna_map_to_feature_plots/intersect_and_count_smallrna.plot.R
@@ -1,13 +1,13 @@
 ## echo "SRS.42A18AT1.br1.tr1.ox.ovary.fq.gz SRS.42A18AT1.br2.tr1.ox.ovary.fq.gz SRS.42A18AT1.br3.tr1.ox.ovary.fq.gz SRS.GS1AT1.br1.tr1.ox.ovary.fq.gz SRS.GS1AT1.br2.tr1.ox.ovary.fq.gz SRS.GS1AT1.br3.tr1.ox.ovary.fq.gz SRS.42A18BT1.br1.tr1_2.ox.ovary.fq.gz SRS.42A18BT1.br2.tr1.ox.ovary.fq.gz SRS.42A18BT1.br3.tr1.ox.ovary.fq.gz SRS.GS1BT1.br1.tr1.ox.ovary.fq.gz SRS.GS1BT1.br2.tr1.ox.ovary.fq.gz SRS.GS1BT1.br3.tr1.ox.ovary.fq.gz SRS.42A18CT1.br1.tr1.ox.ovary.fq.gz SRS.42A18CT1.br2.tr1.ox.ovary.fq.gz SRS.42A18CT1.br3.tr1.ox.ovary.fq.gz SRS.GS1CT1.br1.tr1.ox.ovary.fq.gz SRS.GS1CT1.br2.tr1.ox.ovary.fq.gz SRS.GS1CT1.br3.tr1_2.ox.ovary.fq.gz SRS.GS1CT1.br3.tr2.ox.ovary.fq.gz" | sed 's/ /\n/g' | sed 's/.fq.gz//' | sed 's/^/"/' | sed 's/$/"/' | tr '\n' ','
-
 libs <- c("SRS.42A18AT1.br1.tr1.ox.ovary","SRS.42A18AT1.br2.tr1.ox.ovary","SRS.42A18AT1.br3.tr1.ox.ovary","SRS.GS1AT1.br1.tr1.ox.ovary","SRS.GS1AT1.br2.tr1.ox.ovary","SRS.GS1AT1.br3.tr1.ox.ovary","SRS.42A18BT1.br1.tr1_2.ox.ovary","SRS.42A18BT1.br2.tr1.ox.ovary","SRS.42A18BT1.br3.tr1.ox.ovary","SRS.GS1BT1.br1.tr1.ox.ovary","SRS.GS1BT1.br2.tr1.ox.ovary","SRS.GS1BT1.br3.tr1.ox.ovary","SRS.42A18CT1.br1.tr1.ox.ovary","SRS.42A18CT1.br2.tr1.ox.ovary","SRS.42A18CT1.br3.tr1.ox.ovary","SRS.GS1CT1.br1.tr1.ox.ovary","SRS.GS1CT1.br2.tr1.ox.ovary","SRS.GS1CT1.br3.tr1_2.ox.ovary")
-
+## CHANGE here to switch between using all mappers or unique mappers
+au = "uniq"
 # libs <- libs[seq(1, 6)]
 df <- data.frame()
 for (i in libs) {
     one.sample <- data.frame()
     for (j in c("transposon", "cluster", "construct")) {
-        fn <- paste(i, ".", j, ".abundance.normalized_by_allxmirna", sep="")
+        fn <- paste(i, ".", au, ".", j, ".abundance.normalized_by_allxmirna", sep="")
         tmp <- read.table(fn)
         if (j == "transposon") {
             colnames (tmp) = c("name", "group", paste(i, ".S", sep=""), paste(i, ".AS", sep=""))
@@ -23,9 +23,17 @@ for (i in libs) {
     if (length(df) == 0) {
         df <- one.sample
     } else {
-        df <- merge(df, one.sample, by = c("name", "group"))
+        # all=TRUE becasue some libraries have nasty 0 count
+        df <- merge(df, one.sample, by = c("name", "group"), all=TRUE)
     }    
 }
+### Some constructs may not be visible because they have no unique mappers
+## For those constructs with NA count, set them to 0
+df[is.na(df)] = 0
+## idx <- df$group=="construct"
+## df.tmp <- df[idx, seq(3, ncol(df))]
+## df.tmp[df.tmp < 1] = 1
+## df[idx, seq(3, ncol(df))] = df.tmp
 
 library(ggplot2)
 library(ggthemes)
@@ -107,6 +115,41 @@ for (g in list(g1.x, g1.y, g2.x, g2.y, g3.x, g3.y)) {
     colnames(df.mean)[ ncol(df.mean) ] <- paste(n, ".SAS", sep="")    
 }
 
+df.p.fdr <- data.frame(name=df$name)
+f <- function(x) {
+    ## t.test(x[c(1,2,3)], x[c(4,5,6)]) [["p.value"]]
+    if (all(x==x[1])) {
+        1
+    } else {
+        t.test(x[c(1,2,3)], x[c(4,5,6)]) [["p.value"]] 
+    }
+}
+
+for(xy in list(list(g1.x, g1.y), list(g2.x, g2.y), list(g3.x, g3.y))) {
+    x <- xy[[1]]
+    n.x <- gsub("(SRS.[^.]+).+", "\\1", x[[1]])[1]
+    y <- xy[[2]]
+    n.y <- gsub("(SRS.[^.]+).+", "\\1", y[[1]])[1]
+    s.x <- df[, paste(x, ".S", sep="")]
+    s.y <- df[, paste(y, ".S", sep="")]
+    as.x <- df[, paste(x, ".AS", sep="")]
+    as.y <- df[, paste(y, ".AS", sep="")]
+    a.xy <- cbind(s.x, s.y)
+    as.xy <- cbind(as.x, as.y)
+    ## Total = sense + antisense. The colnames are not accurate here...
+    sas.xy <- a.xy + as.xy
+    ## Add a very small random number to every element so that t.test does not complain...
+    p <- apply(sas.xy, 1, f)
+    p.adj <- p.adjust(p, method="fdr")
+    p.adj <- data.frame(p.adj)
+    colnames(p.adj) <- paste(n.x, ".vs.", n.y, ".fdr", sep="")
+    p <- data.frame(p)
+    colnames(p) <- paste(n.x, ".vs.", n.y, ".p", sep="")
+    df.p.fdr <- cbind(df.p.fdr, p)        
+    df.p.fdr <- cbind(df.p.fdr, p.adj)
+}
+
+write.table(df.p.fdr, paste(au, ".scatterplots.transposon.cluster.construct.p.fdr.table.txt", sep=""), quote=F, sep="\t", row.names=FALSE)
 
 lim <- max( df.mean[, seq(3, ncol(df.mean))])
 lim <- 10^ceiling(log10(lim))
@@ -118,7 +161,7 @@ idx <- grepl("cluster", df.mean$group)
 df.mean.cluster <- df.mean[idx, ]
       
 
-pdf("scatterplots.transposon.cluster.construct.pdf")
+pdf(paste(au, ".scatterplots.transposon.cluster.construct.pdf", sep=""))
 for (i in list( list(g1.x.lab, g1.y.lab), list(g2.x.lab, g2.y.lab), list(g3.x.lab, g3.y.lab))) {
     x <- i[[1]]
     y <- i[[2]]
@@ -134,3 +177,5 @@ for (i in list( list(g1.x.lab, g1.y.lab), list(g2.x.lab, g2.y.lab), list(g3.x.la
     }
 }
 dev.off()
+
+write.table(df, file=paste(au, ".scatterplots.transposon.cluster.construct.table.txt", sep=""), quote=F, sep="\t", row.names=FALSE)
